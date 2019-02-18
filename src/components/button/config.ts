677029134c2aaf6c07f9da0f46450c711da7f0b5
dblink <1203683833@qq.com>
2019-02-18 13:43:57 +0800

    update

--- src/components/button/config.ts
+++ src/components/button/config.ts
@@ -5,35 +5,5 @@ export type config = {
     icon ?: BaseButtonProps;
 }
 export const config: config = {
-    windPrimaryButton: {
-        style: {
-            height: '36px',
-            maxWidth: '320px',
-            width: '90%',
-            lineHeight: '36px',
-            textAlign: "center",
-            display: 'inline-block',
-            borderRadius: '18px',
-            cursor: 'pointer',
-            color: '#333333',
-            margin: 'auto',
-            backgroundColor: '#FBDE3C',
-            fontSize: '14px',
-            fontWeight: 'bold',
-            boxShadow: '0 5px 10px rgba(255, 222, 60, .5)'
-        },
-        mouseDown: {
-            backgroundColor: '#F2CF0B'
-        }
-    },
-    icon: {
-        style: {
-            verticalAlign: 'middle',
-            display: 'inline-block',
-            textAlign: 'center',
-            background: '#ffde00',
-            borderRadius: '50%',
-            fontWeight: 'bold'
-        }
-    }
+    
 };
--- src/components/input/config.ts
+++ src/components/input/config.ts
@@ -1,50 +1,8 @@
 import { BaseInputProps } from './base/input';
 
 type config = {
-    userInput: BaseInputProps;
-    userInputPassword: BaseInputProps;
+    userInput ?: BaseInputProps;
+    userInputPassword ?: BaseInputProps;
 }
 export const config:config = {
-    userInput: {
-        mouseFocus: {
-            // border: '1px solid #FBDE3C'
-        },
-        style: {
-            height: '36px',
-            maxWidth: '320px',
-            width: '90%',
-            lineHeight: '36px',
-            display: 'inline-block',
-            borderRadius: '18px',
-            color: '#666666',
-            backgroundColor: '#f2f2f2',
-            margin: 'auto',
-            fontSize: '12px',
-            border: 'none',
-            textIndent: '20px'
-        },
-        type: 'text'
-    },
-    userInputPassword: {
-        mouseFocus: {
-            // border: '1px solid #FBDE3C'
-        },
-        style: {
-            height: '36px',
-            maxWidth: '320px',
-            width: '90%',
-            lineHeight: '36px',
-            display: 'inline-block',
-            borderRadius: '18px',
-            color: '#666666',
-            WebkitAppearance: 'none',
-            appearance: 'none',
-            backgroundColor: '#f2f2f2',
-            fontSize: '12px',
-            margin: 'auto',
-            border: 'none',
-            textIndent: '20px'
-        },
-        type: 'password'
-    }
 };
